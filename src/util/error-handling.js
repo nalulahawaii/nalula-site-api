b4002c5425877fcd8896b4734ac3eb5c6d1c1f2b
Charles Koehl <charles@etherscapes.com>
2022-07-04 19:00:33 -1000

    chore: set up sentry.io

--- src/util/error-handling.js
+++ src/util/error-handling.js
@@ -1,11 +1,15 @@
+import * as Sentry from '@sentry/node'
 import { newLogger } from 'src/services/logging'
 import { logValDetailed } from 'src/util/debug'
 
 const log = newLogger('Development Error Logging')
-log.level = 'info'
 
 if(process.env.NODE_ENV === 'development') log.info('Sentry.io-bound errors will be logged here in development environment')
 
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  tracesSampleRate: 1.0,
+})
 /**
  * Send error report to error logging service if on production, or just log it.
  *
@@ -15,9 +19,7 @@ if(process.env.NODE_ENV === 'development') log.info('Sentry.io-bound errors will
 export const reportError = ({ e, ...context }) => {
   const err = typeof e === 'string' ? new Error(e) : e
   if(process.env.NODE_ENV === 'production') {
-    // TODO: Enable when Sentry.io account created.
-    // Sentry.captureException(err, context)
-    log.error(err, logValDetailed(context))
+    Sentry.captureException(err, context)
   } else {
     log.error(err, logValDetailed(context))
   }
